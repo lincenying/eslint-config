3f291c93de77c28f8b9575197665adde1815a82d
林岑影 <lincenying@qq.com>
2023-08-24 10:53:05 +0800

    chore: async rules

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -1,3 +1,6 @@
+const isInEditor = (process.env.VSCODE_PID || process.env.JETBRAINS_IDE) && !process.env.CI
+const offInEditor = isInEditor ? 'off' : 'error'
+
 module.exports = {
     env: {
         es6: true,
@@ -42,6 +45,8 @@ module.exports = {
         '!.github',
         '!.vitepress',
         '!.vscode',
+        // force exclude
+        '**/.vitepress/cache',
     ],
     plugins: [
         'html',
@@ -137,6 +142,14 @@ module.exports = {
                         order: { type: 'asc' },
                     },
                     {
+                        pathPattern: '^resolutions$',
+                        order: { type: 'asc' },
+                    },
+                    {
+                        pathPattern: '^pnpm.overrides$',
+                        order: { type: 'asc' },
+                    },
+                    {
                         pathPattern: '^exports.*$',
                         order: [
                             'types',
@@ -220,7 +233,7 @@ module.exports = {
         'quotes': ['error', 'single'],
         'quote-props': ['error', 'consistent-as-needed'],
 
-        'unused-imports/no-unused-imports': 'error',
+        'unused-imports/no-unused-imports': offInEditor,
         'unused-imports/no-unused-vars': [
             'warn',
             {
