64e5e0f861f32653ef27f031c5eef8791933e488
林岑影 <lincenying@qq.com>
2023-09-22 15:13:27 +0800

    chore: 关闭no-console

--- eslint.config.js
+++ eslint.config.js
@@ -3,18 +3,18 @@ import sortKeys from 'eslint-plugin-sort-keys'
 import eslintConfig from '@lincy/eslint-config'
 
 const config = eslintConfig(
-  undefined,
-  {
-    files: ['**/eslint-config/src/**/*.ts'],
-    plugins: {
-      '@stylistic/migrate': stylisticMigrate,
-      'sort-keys': sortKeys,
+    undefined,
+    {
+        files: ['**/eslint-config/src/**/*.ts'],
+        plugins: {
+            '@stylistic/migrate': stylisticMigrate,
+            'sort-keys': sortKeys,
+        },
+        rules: {
+            '@stylistic/migrate/rules': 'error',
+            'sort-keys/sort-keys-fix': 'error',
+        },
     },
-    rules: {
-      '@stylistic/migrate/rules': 'error',
-      'sort-keys/sort-keys-fix': 'error',
-    },
-  },
 )
 
 export default config
