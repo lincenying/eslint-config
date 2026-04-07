92690d0baa7161454156437425d90c03c4ef0d14
LinCenYing <lincenying@qq.com>
2026-04-07 15:05:48 +0800

    feat: 添加对 TypeScript 文件的忽略功能
    函数 `ignores` 现在接受两个参数：`userIgnores` 和 `ignoreTypeScript`。
    - `userIgnores` 可以是一个字符串数组或一个函数，该函数接收原始忽略数组并返回一个新的忽略数组。
    - `ignoreTypeScript` 是一个布尔值，用于决定是否忽略 TypeScript 文件。
    
    在函数内部，根据 `ignoreTypeScript` 的值，决定是否将 TypeScript 文件的 glob 模式（`GLOB_TS` 和 `GLOB_TSX`）添加到忽略数组中。
    
    如果 `userIgnores` 是一个函数，则调用该函数并传入当前的忽略数组，得到一个新的忽略数组。
    如果 `userIgnores` 是一个字符串数组，则将其内容合并到当前的忽略数组中。

--- src/configs/ignores.ts
+++ src/configs/ignores.ts
@@ -1,18 +1,31 @@
-import type { OptionsIgnores, TypedFlatConfigItem } from '../types'
+import type { TypedFlatConfigItem } from '../types'
 
-import { GLOB_EXCLUDE } from '../globs'
+import { GLOB_EXCLUDE, GLOB_TS, GLOB_TSX } from '../globs'
 
-export async function ignores(options: OptionsIgnores = {}): Promise<TypedFlatConfigItem[]> {
-    const {
-        ignores = [],
-    } = options
+export async function ignores(
+    userIgnores: string[] | ((originals: string[]) => string[]) = [],
+    ignoreTypeScript = false,
+): Promise<TypedFlatConfigItem[]> {
+    let ignores = [
+        ...GLOB_EXCLUDE,
+    ]
+
+    if (ignoreTypeScript)
+        ignores.push(GLOB_TS, GLOB_TSX)
+
+    if (typeof userIgnores === 'function') {
+        ignores = userIgnores(ignores)
+    }
+    else {
+        ignores = [
+            ...ignores,
+            ...userIgnores,
+        ]
+    }
 
     return [
         {
-            ignores: [
-                ...GLOB_EXCLUDE,
-                ...ignores,
-            ],
+            ignores,
             name: 'eslint/ignores',
         },
     ]
